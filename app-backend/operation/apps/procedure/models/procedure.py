f20c61dfc802b6d59575f0db574a857473e7ca8a
Fabio Cordoba <fabiocordoba1@gmail.com>
2026-05-10 16:07:04 -0500

    feat: add procedure app with initial model and migrations

---
+++ app-backend/operation/apps/procedure/models/procedure.py
@@ -0,0 +1,17 @@
+from django.db import models
+from apps.core.models.base_model import BaseModel
+
+class Procedure(BaseModel):
+
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        ACTIVE = 'active', 'Active'
+        COMPLETED = 'completed', 'Completed'
+        CANCELLED = 'cancelled', 'Cancelled'
+
+    vehicle_id = models.IntegerField()
+    customer_id = models.IntegerField()
+    request_date = models.DateField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
